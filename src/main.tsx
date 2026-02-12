88eda45bc9ee3f616d23f71d666c8b1284dad479
markosneiders <markosneiders@gmail.com>
2026-02-12 17:57:44 +0200

    Added a polyfill for requestIdleCallback

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,17 @@
+// Polyfill for Safari (doesn't support requestIdleCallback)
+if (typeof window !== 'undefined' && !window.requestIdleCallback) {
+  window.requestIdleCallback = (cb: IdleRequestCallback) => {
+    const start = Date.now();
+    return window.setTimeout(() => {
+      cb({
+        didTimeout: false,
+        timeRemaining: () => Math.max(0, 50 - (Date.now() - start)),
+      });
+    }, 1);
+  };
+  window.cancelIdleCallback = (id: number) => clearTimeout(id);
+}
+
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
